1fcd8608c69aa9ee8d0c7270087e704d2515fad4
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2023-06-18 19:58:24 +0200

    Added program content

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -1,5 +1,31 @@
 import React from "react";
 
+import rightArrow from "../assets/rightArrow.png";
+import { programsData } from "../data/programsData";
+import "./Programs.css";
 export default function Programs() {
-  return "Hello world";
+  return (
+    <div className="Programs">
+      <div className="programs-headings">
+        <span className="stroke-text">explore</span>
+        <span>programs</span>
+        <span className="stroke-text">to shape you</span>
+      </div>
+      <div className="programs-content">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <span>{program.heading}</span>
+            <span>{program.details}</span>
+            <div className="join-now">
+              {" "}
+              <span>
+                Join now <img src={rightArrow} alt="right arrow" />
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
