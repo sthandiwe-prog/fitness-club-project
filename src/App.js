d4bc6c78a866e3eb4047efd275bb854cc6662586
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2023-06-18 16:24:49 +0200

    Added programs section

--- src/App.js
+++ src/App.js
@@ -1,81 +1,12 @@
-import Navigation from "./Navigation";
-import Heart from "./assets/heart.png";
-import Calories from "./assets/calories.png";
-import Hero from "./assets/hero_image.png";
-import Hero_back from "./assets/hero_image_back.png";
+import Hero from "./Hero";
+import Programs from "./Programs";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="left-header-content">
-        <Navigation />
-        <div className="moto">
-          <div></div>
-          <span>the best fitness club in the town</span>
-        </div>
-        <div className="header-text">
-          <div>
-            <span className="stroke-text">shape </span>
-            <span>your</span>
-          </div>
-          <div>
-            <span>ideal </span>
-            <span>body</span>
-          </div>
-          <div>
-            <span>
-              In here we will help you to shape and build your ideal body and
-              live up your life to fullest
-            </span>
-          </div>
-        </div>
-        <div className="figures">
-          <div>
-            {" "}
-            <span>+ 140</span>
-            <span>expert coaches</span>
-          </div>
-          <div>
-            {" "}
-            <span>+ 978</span>
-            <span>members joined</span>
-          </div>
-          <div>
-            {" "}
-            <span>+ 50</span>
-            <span>fitness programs</span>
-          </div>
-        </div>
-        <div className="header-buttons">
-          <button>Get Started</button>
-          <button>Learn More</button>
-        </div>
-      </div>
-      <div className="right-header-content">
-        <button className="header-button">Join Now</button>
-        <div className="heart-rate">
-          <div>
-            {" "}
-            <img src={Heart} alt="heart" />
-          </div>
-          <div>
-            <span>heart rate</span>
-            <span>116 bpm</span>
-          </div>
-        </div>
-        <div className="gym-logo">
-          <img src={Hero} alt="gymic" />
-          <img src={Hero_back} alt="web design" />
-          <div className="calories-content">
-            <div> </div>
-            <div>
-              <span>calories burned</span>
-              <span>220 kcal</span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Hero />
+      <Programs />
     </div>
   );
 }
